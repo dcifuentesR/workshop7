015edf3ff969dc8d2352d4911677bd60959e6a1d
dicufentesr <danixbox1313@gmail.com>
2019-02-28 08:27:37 -0500

    íntegré proyecto anterior e implementé CinemaAPIController

--- src/main/java/edu/eci/arsw/cinema/controllers/CinemaAPIController.java
+++ src/main/java/edu/eci/arsw/cinema/controllers/CinemaAPIController.java
@@ -5,9 +5,15 @@
  */
 package edu.eci.arsw.cinema.controllers;
 
+import edu.eci.arsw.cinema.persistence.CinemaPersistenceException;
+import edu.eci.arsw.cinema.services.CinemaServices;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author cristian
  */
+@RestController
+@RequestMapping(value="/cinema")
 public class CinemaAPIController {
     
+    @Autowired
+    CinemaServices cs;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity manejadorGetCinema(){
+        try {
+            return new ResponseEntity(cs.getAllCinemas(), HttpStatus.ACCEPTED);
+        } catch (CinemaPersistenceException ex) {
+            Logger.getLogger(CinemaAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity("No se encontraron los cinemas",HttpStatus.NOT_FOUND);
+        }
+    }
 }
