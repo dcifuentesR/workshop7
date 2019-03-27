c80a4833acae26d33a4530042c4b7cb3924981e0
dcifuentesr <danixbox1313@gmail.com>
2019-03-27 16:35:24 -0500

    segundo mapeo

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -33,17 +33,28 @@ var ControllerModule = (function(){
     	};
     	
     	updateCinemaFunctions:function(cinemaName){
+    		var table= $(#Cinemas);
+    		table.empty();
     		selectCinema(cinemaName);
     		//----------------------Map to reduced Object----------------
     		reducedFunctions = selectedCFunctions.map(function(func){
 					    			var reducedFunction={};
 					    			var numSeats=0;
+					    			reducedFunction["cinema"]=selectedCinemaName;
 					    			reducedFunction["name"]=func.movie.name;
 					    			reducedFunction["seats"]=func.seats.foreach(getAvailableSeatsInRow(row,numSeats));
 					    			reducedFunction["date"]=func.date;
 					    			return reducedFunction;
 					    		});
+    		//---------------------Map to table---------------------------
+    		reducedFunctions.foreach(function(funct){
+    			table.append("<tr><td>"+funct.cinema+"</td>"
+    						+"<td>"+funct.name+"</td>"
+    						+"<td>"+funct.seats+"</td>"
+    						+"<td>"+funct.date+"</td></tr>");
+    		});
     		
+    		table.append();
     		
     	};
     }
